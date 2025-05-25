c1ea057406d553365cef3c71caba1b40f6561d2b
Eidrien2810 <138817501+Eidrien2810@users.noreply.github.com>
2025-05-25 19:21:52 -0400

    feat: estructura inicial de la página principal, agrega header, hero, sección 'New Watches' y estilos base para layout y productos

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+const images = [
+  {
+    imgURL: 'https://static.wixstatic.com/media/dd32b2_710d4885995a4b3292dd80ed1c78e423~mv2.png/v1/fill/w_304,h_228,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/dd32b2_710d4885995a4b3292dd80ed1c78e423~mv2.png',
+    nameTag: 'Reloj Elegante Rosa',
+    price: 'RD$95.00'
+  }
+]
